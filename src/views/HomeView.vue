42b9175f2714bebe10a250c80f00c98c6ca90068
Zjelco Felder <zfe@mediaan.com>
2025-01-22 11:58:57 +0100

    setup vue router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is an home page</h1>
+    <RouterLink to="/projects">Projects</RouterLink>
+  </div>
 </template>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="projects">
+    <h1>This is an projects page</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
