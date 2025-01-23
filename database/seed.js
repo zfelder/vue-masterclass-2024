e1a130a785d5c7fa0d7802c8ab0094e6f4081cad
Zjelco Felder <zfe@mediaan.com>
2025-01-23 12:37:13 +0100

    seed db

--- database/seed.js
+++ database/seed.js
@@ -10,9 +10,21 @@ dotenv.config()
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_KEY)
 
-// Add a simple seed function to use faker (resolving the unused import warning)
-async function seedDatabase() {
-  console.log(supabase)
+const seedProjects = async (numEntries) => {
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+    })
+  }
+
+  await supabase.from('projects').insert(projects)
 }
 
-seedDatabase().catch(console.error)
+seedProjects(10)
