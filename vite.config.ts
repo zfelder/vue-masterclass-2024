42a140c7e2ec82df4616f908d1e5f74c0680c5e7
Zjelco Felder <zfe@mediaan.com>
2025-02-25 14:40:25 +0100

    4.33-getting-started-with-shadcn-ui-and-vue-js

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 // https://vite.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
     // vueDevTools(),
