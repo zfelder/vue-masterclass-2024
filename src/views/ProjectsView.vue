8956cc7ff8762c9b8d5f58ed251a7c9715dd50d6
Zjelco Felder <zfe@mediaan.com>
2025-02-25 13:04:18 +0100

    query-supabase-from-vue

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="projects">
     <h1>This is an projects page</h1>
+    <div v-for="project in projects" :key="project.id">
+      <h2>{{ project.name }}</h2>
+    </div>
     <RouterLink to="/">Home</RouterLink>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+
+const getProjects = async () => {
+  const { data, error } = await supabase.from('projects').select('*')
+
+  if (error) {
+    console.error(error)
+  }
+
+  return data
+}
+
+const projects = ref([])
+
+onMounted(async () => {
+  projects.value = await getProjects()
+})
+</script>
