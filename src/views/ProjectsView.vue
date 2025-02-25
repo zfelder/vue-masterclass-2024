0b60edfc231d7c1c607a537a6bdb35762fd92fbb
Zjelco Felder <zfe@mediaan.com>
2025-02-25 13:50:40 +0100

    3.30-provide-typescript-type-definitions-for-refs-in-vue-js-script-setup

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,7 +2,13 @@
   <div class="projects">
     <h1>This is an projects page</h1>
 
-    {{ projects }}
+    {{ projects ? projects[0] : '' }}
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h2>{{ project.name }}</h2>
+      </li>
+    </ul>
 
     <RouterLink to="/">Home</RouterLink>
   </div>
@@ -12,13 +18,12 @@
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 
-const projects = ref([])
+const projects = ref<any[] | null>(null)
 
 ;(async () => {
   const { data, error } = await supabase.from('projects').select()
 
   if (error) console.error(error)
-  console.log(data)
 
   projects.value = data
 
