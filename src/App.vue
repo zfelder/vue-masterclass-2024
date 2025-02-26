1b2993b41a521aa894d8eed3125c370d195614da
Zjelco Felder <zfe@mediaan.com>
2025-02-26 09:20:55 +0100

    4.35-utilize-shadcn-input-and-dropdown-components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+</script>
 
 <template>
   <nav class="flex h-16 items-center justify-between border-b bg-muted/40 px-8">
     <form class="w-full max-w-96">
-      <input class="w-full bg-background pl-8" type="text" placeholder="Search..." />
+      <Input class="w-full bg-background pl-8" type="text" placeholder="Search..." />
       <!-- avatar -->
     </form>
-    <div class="h-8 w-8 rounded-full bg-white"></div>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        ><Avatar>
+          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar></DropdownMenuTrigger
+      >
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,6 @@
   <div class="home">
     <h1>This is an home page</h1>
     <RouterLink to="/projects">Projects</RouterLink>
+    <RouterLink to="/tasks">Tasks</RouterLink>
   </div>
 </template>
